cda017a59163b3cc067e94a0734516acc95c0973
vinicius  Rodrigues Doimo <viniciusrodriguesdoimo@viniciuss-MacBook-Air.local>
2023-05-31 07:57:06 -0300

    Initial scope

---
+++ src/main/kotlin/com/vinicius/rodrigues/doimo/calendarmanagerapi/CalendarManagerApiApplication.kt
@@ -0,0 +1,11 @@
+package com.vinicius.rodrigues.doimo.calendarmanagerapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CalendarManagerApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<CalendarManagerApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/vinicius/rodrigues/doimo/calendarmanagerapi/CalendarManagerApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.vinicius.rodrigues.doimo.calendarmanagerapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CalendarManagerApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
